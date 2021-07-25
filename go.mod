c4786aa807491596b49b3e5377975b1cb04e98bb
Trevor Roberts Jr <Trevor@DevWidgets.io>
2021-07-25 19:36:51 -0400

    add AWS SDK dependencies and boilerplate Go Lambda code

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/trevorrobertsjr/go-lambda-sqs-consumer
 
 go 1.16
+
+require (
+	github.com/aws/aws-lambda-go v1.24.0
+	github.com/aws/aws-sdk-go v1.39.0
+	github.com/stretchr/testify v1.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"context"
+	"encoding/json"
+	"log"
 
+	"github.com/aws/aws-lambda-go/events"
+	runtime "github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+func handleRequest(ctx context.Context, event events.CodePipelineEvent) (string, error) {
+	// event
+	eventJson, _ := json.MarshalIndent(event, "", "  ")
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	})
+	if err != nil {
+		log.Println("Failed to create bucket", err)
+		return "error", err
+	}
+
+	// Create an SQS client
+	sqsClient := sqs.New(sess)
+
+	// Create a DynamoDB client
+	dynamodbClient := dynamodb.New(sess)
+
+	return string(eventJson), nil
+}
+
+func main() {
+	runtime.Start(handleRequest)
 }
